7501931fc1a47b5847101408b1176808950843f2
CakeboyDev <emerthedev@gmail.com>
2021-04-19 23:36:03 -0300

    Jogo finalizado

--- javascript/cobrinha.js
+++ javascript/cobrinha.js
@@ -7,10 +7,19 @@ snake[0] = {
   y: 8*box
 }
 let direction = 'right';
+let food={
+  x: Math.floor(Math.random()*15+1)*box,
+  y: Math.floor(Math.random()*15+1)*box
+}
 
 function criarBG(){
 context.fillStyle='lightgreen';
 context.fillRect(0,0,16*box,16*box);
+}
+
+function drawFood(){
+  context.fillStyle='red';
+  context.fillRect(food.x, food.y, box, box);
 }
 
 function criarCobra(){
@@ -34,8 +43,16 @@ function iniciarJogo(){
   if(snake[0].y>15*box && direction=='down')snake[0].y=0;
   if(snake[0].y<0 && direction=='up')snake[0].y=16*box;
 
+  for(i=1; i<snake.length;i++){
+    if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+      clearInterval(jogo);
+      alert('Game Over :D');
+}
+}
+
   criarBG();
   criarCobra();
+  drawFood();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
@@ -45,12 +62,17 @@ function iniciarJogo(){
   if(direction=='up') snakeY-=box;
   if(direction=='down') snakeY+=box;
 
+  if(snakeX !=food.x || snakeY !=food.y){
   snake.pop();
-let newHead = {
+  }
+  else{food.x= Math.floor(Math.random()*15+1)*box;
+  food.y= Math.floor(Math.random()*15+1)*box;
+  }
+
+  let newHead = {
   x: snakeX,
   y: snakeY
-}
+  }
   snake.unshift(newHead);
 }
-
 let jogo = setInterval(iniciarJogo, 100);
